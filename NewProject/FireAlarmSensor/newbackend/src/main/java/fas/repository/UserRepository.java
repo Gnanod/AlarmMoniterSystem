f8ff191899f0627e28b64a9cddc0d4826b073cb4
gnanod <1995gnanod@gmail.com>
2020-05-06 09:11:26 +0530

    no message

--- NewProject/FireAlarmSensor/newbackend/src/main/java/fas/repository/UserRepository.java
+++ NewProject/FireAlarmSensor/newbackend/src/main/java/fas/repository/UserRepository.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    //Check User Login Credentials
     @Query(value = "from User where username =?1 and password=?2")
     User loginUser(String username, String password);
 
+    //Get Logged User Details
     @Query(value = "from User where userStatus ='Logged'")
     List<User> getLoggedUserDetails();
 }
